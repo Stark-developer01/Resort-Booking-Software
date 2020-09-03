1d9a62744c4a95caef48705ba6dffc195ef52a01
DEBARGHA SAHA <54001098+Stark-developer01@users.noreply.github.com>
2020-09-03 16:40:30 +0530

    initial_edits

---
+++ src/components/Services.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {FaCocktail, FaShuttleVan, FaHiking, FaBeer} from "react-icons/fa";
+import Title from "./Title";
+
+export default class Services extends Component {
+    state={
+        services:[
+            {
+                icon:<FaCocktail/>,
+                title:"Free Cocktails",
+                info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, architecto?"
+            },
+            {
+                icon: <FaHiking/> ,
+                title: "Endless Hiking",
+                info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, architecto?"
+            },
+            {
+                icon: <FaShuttleVan/> ,
+                title: "Free Shuttle",
+                info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, architecto?"
+            },
+            {
+                icon: <FaBeer/> ,
+                title: "Strongest Beer",
+                info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, architecto?"
+            }
+        ]
+    };
+
+    render() {
+        return (
+            <section className="services">
+                <Title title="services"/>
+                <div className="services-center">
+                    {this.state.services.map((item, index) => {
+                        return (
+                        <article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                        );
+                    })}
+                </div>
+            </section>
+        );
+    }
+}
+
---
+++ src/pages/Rooms.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from "react-router-dom";
+
+
+const Rooms = () => {
+    return (
+    <Hero hero="roomsHero">
+        <Banner title="Our Rooms">
+            <Link to="/" className="btn-primary">
+                Return Home
+            </Link>
+        </Banner>
+    </Hero>
+    );
+};
+
+export default Rooms;
